098b0d1ec02a98ba0defbcbe7445f18472548dae
iamthedarksaint <bojzino128@gmail.com>
2025-08-25 00:46:06 +0100

    added new function in my hello file

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 
 def hello():
-    name = input("Please enter your name for a warm welcome.\n")
+    name = input("Please enter your name for a warm welcome: \n")
     return print(f"Hello omoyami, {name}!")
 
+def add():
+    a = input("Please input first numbers: \n")
+    b = input("Input the second number: \n")
+    c = a + b
+    return print(f"The addition of both number gives {c}.")
 hello()
+add()
